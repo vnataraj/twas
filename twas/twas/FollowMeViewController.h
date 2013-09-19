455513e4299115ffef48dddc45ede2811f36c323
Vipul Nataraj <vnataraj@purdue.edu>
2013-09-19 01:13:34 -0400

    updated for ios7

--- twas/twas/FollowMeViewController.h
+++ twas/twas/FollowMeViewController.h
@@ -11,4 +11,7 @@
 @interface FollowMeViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UILabel *followLabel;
 - (IBAction)followSelected:(id)sender;
+@property (weak, nonatomic) UITextField *followerSearchField;
+@property (weak, nonatomic) UILabel *search;
+- (IBAction)searchTwitter:(id)sender;
 @end
